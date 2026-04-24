b884b57d7b619e46d8c18cf998af02256c173a3e
ChoiT815 <choi081517@gmail.com>
2026-04-24 15:44:58 +0900

    refactor: 고객 엔티티 롬복 적용

--- src/main/java/com/commerceapp/customers/entity/Customers.java
+++ src/main/java/com/commerceapp/customers/entity/Customers.java
@@ -49,30 +49,4 @@ public class Customers extends BaseEntity {
     public void updateStatus(String status) {
         this.status = status;
     }
-
-    /*========== Getter ===========*/
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getStatus() {
-        return status;
-    }
 }
